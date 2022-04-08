0d66c2a9864ee4d950470ce4a6aa5f59f8cac6d3
Elliot Rask <elliot.rask@qliro.com>
2022-04-08 10:10:55 +0200

    Update shipping and payment method types

--- src/models/PaymentMethod.ts
+++ src/models/PaymentMethod.ts
@@ -1,5 +1,6 @@
 export interface PaymentMethod {
-  method?: string;
-  subtype?: string;
+  method: string;
+  subtype: string;
   price: number;
+  priceExVat: number;
 }
--- src/models/Shipping.ts
+++ src/models/Shipping.ts
@@ -1,5 +1,9 @@
 export interface Shipping {
   method: string;
   secondaryOption: string;
+  additionalShippingServices: string[];
   price: number;
+  priceExVat: number;
+  totalShippingPrice: number;
+  totalShippnigPriceExVat: number;
 }
